6a1c38f2cee5183921c70aace62dc7ef488238af
bbalage <bbalage0@gmail.com>
2022-03-15 11:35:39 +0100

    Added frame for frontend.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CanvasComponent } from './main-screen/canvas/canvas.component';
+import { ChatComponent } from './main-screen/chat/chat.component';
+import { InfoBarComponent } from './main-screen/info-bar/info-bar.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainScreenComponent,
+    CanvasComponent,
+    ChatComponent,
+    InfoBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
