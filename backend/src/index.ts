60c83f108bf6249797cbbdf080a78307e69eb15f
DavidPolonkai <david.polonkai2@gmail.com>
2022-05-18 16:40:11 +0200

    HOTFIX: error during merge of admin-component, sqlite_#9, websocket-prototype and master

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,8 +13,6 @@ const app = express();
 
 //initialize a simple http server
 const server = http.createServer(app);
-//initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
 
 const allowedOrigins = ["http://localhost:4200"];
 
@@ -24,18 +22,6 @@ const options: cors.CorsOptions = {
 
 app.use(cors(options));
 app.use(express.json());
-
-wss.on("connection", (ws: WebSocket) => {
-  //connection is up, let's add a simple simple event
-  ws.on("message", (message: string) => {
-    //log the received message and send it back to the client
-    console.log("received: %s", message);
-    ws.send(`Hello, you sent -> ${message}`);
-  });
-
-  //send immediatly a feedback to the incoming connection
-  ws.send("Hi there, I am a WebSocket server");
-});
 
 //start our server
 const port: number = 3000;
